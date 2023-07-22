c1868907b780d3ca6d9df9ec308779ea88df64d8
shojol021 <shojol021@gmail.com>
2023-07-23 03:26:42 +0600

    typing effect applied

--- dom.js
+++ dom.js
@@ -26,3 +26,39 @@ function slideNext() {
 }
 
 slideTimer = setInterval(slideNext, 5000);
+
+
+
+// ================================== Typing effect start ==============================
+
+// ================================== Typing effect start ==============================
+
+let title = document.getElementById('typing');
+let text = "Welcome To GeneCTech";
+let index = 0;
+let coloredText = "";
+
+const typing = () => {
+    if (index <= text.length) {
+        if (index === 15) {
+            coloredText += '<span class="primary-color">' + text.charAt(index) + '</span>';
+        } else {
+            coloredText += text.charAt(index);
+        }
+
+        title.innerHTML = coloredText;
+        index++;
+
+        if (index <= text.length) {
+            setTimeout(typing, 100);
+        } else {
+            // Reset index and coloredText to restart the typing effect
+            index = 0;
+            coloredText = "";
+            setTimeout(typing, 3000); // Wait for 1 second before restarting the typing effect
+        }
+    }
+}
+
+typing();
+
